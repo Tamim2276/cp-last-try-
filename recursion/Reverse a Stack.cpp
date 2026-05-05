e8de2e7a09c454b4acb742c986b163bfaf041989
Tamim2276 <tamim2276007@gmail.com>
2026-05-05 11:18:55 +0600

    gg

---
+++ recursion/Reverse a Stack.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    void insertButtom(stack<int> &st, int tp)
+    {
+
+        if (st.empty())
+        {
+            st.push(tp);
+            return;
+        }
+
+        int curTp = st.top();
+        st.pop();
+        insertButtom(st, tp);
+        st.push(curTp);
+    }
+    void reverseStack(stack<int> &st)
+    {
+        // code here
+        if (st.empty())
+            return;
+        int tp = st.top();
+        st.pop();
+        reverseStack(st);
+        insertButtom(st, tp);
+    }
+};
